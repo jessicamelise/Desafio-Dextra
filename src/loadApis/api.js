00cc81113f703ab939cf1480f7ee3339fb874610
jessicamelise <jessica.melise@hotmail.com>
2020-10-05 16:48:03 -0300

    alterando url da api de http para https

--- src/loadApis/api.js
+++ src/loadApis/api.js
@@ -1,6 +1,6 @@
 export const loadComicsApi = async (offset) => {
   try {
-    const getComicsApi = await fetch(`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=8420b5759538c93703a1aa3af67a2199&hash=db6ec985bba99cf00b9fa4140ed9340e&limit=10&offset=${offset}`)
+    const getComicsApi = await fetch(`https://gateway.marvel.com/v1/public/comics?ts=1&apikey=8420b5759538c93703a1aa3af67a2199&hash=db6ec985bba99cf00b9fa4140ed9340e&limit=10&offset=${offset}`)
     const response = await getComicsApi.json();
     return response;
   }
