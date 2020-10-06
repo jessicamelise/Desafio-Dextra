659879ea201a1e15cccf0b27a36204f62576bd43
jessicamelise <jessica.melise@hotmail.com>
2020-10-06 16:46:28 -0300

    adição de paginação e gride de 10 em 10 por página

--- src/components/Comics.js
+++ src/components/Comics.js
@@ -1,57 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import '../App.css';
 import { Header } from "../components/Header";
-import { loadComicsApi } from "../loadApis/api.js";
-import { Button } from "./Button.js";
+import { FooterComics } from "./FooterComics.js";
+import { ComicsList } from "./ComicsList.js";
+import { useHistory } from "react-router-dom";
 
 export const Comics = () => {
-  const [comics, setComics] = useState([]);
+  let [page, setPage] = useState(0);
+  let history= useHistory();
+  // let [path, setPath] = useState("");
 
-  // const handleClick = (path) => {
-  //   history.push(path);
-  // };
+  // useEffect(() => {
+  //   history.listen((location) => {
+  //     if (location.pathname === "/home" || location.pathname === "" || location.pathname === "/") {
+  //       setPath("0");
+  //     } else {
+  //       let pathPage = location.pathname.split("/")[2];
+  //       setPath(pathPage);
+  //     }
+  //   })
+  // }, [history])
 
-  useEffect(() => {
-    loadComicsApi(16).then((list) => {
-      setComics(list.data.results);
-    });
-  }, []);
+  const handleClick = (condition) => {
+    if (condition === "Previous") {
+      if (page === 0) {
+        setPage(0);
+      } else {
+        setPage(page-=1);
+        // history.push(`/home/${page}`);
+      }
+    } else {
+      setPage(page+=1);
+      // history.push(`/home/${page}`);
+    }   
+  };
 
   return (
     <>
       <Header />
       <section className="main-comics">
-        <div className="max-width-main">
-          {comics.map((eachComic, index) => (
-            <div className="each-comic" key={index}>
-              <img
-                alt={eachComic.title}
-                src={`${eachComic.thumbnail.path}.${eachComic.thumbnail.extension}`}
-                className="thumbnails"
-              >
-              </img>
-              <p className="p-margin p-font-bold">{eachComic.title}</p>
-              {eachComic.creators.items.length === 0 ?
-                <p className="p-margin"></p> :
-                <p className="p-margin p-font-body">
-                  {eachComic.creators.items.map((creator, index) => <span className="list-creator" key={index}>{creator.name}/ </span>)}
-                </p>
-              }
-            </div>
-          ))}
-        </div>
-        <div className="max-width-button">
-          <Button
-            className="button-comics"
-            name="Previous"
-            // onClick={() => handleClick(parameter)}
-          />
-          <Button
-            className="button-comics"
-            name="Next"
-          // onClick={() => handleClick(parameter)}
-          />
-        </div>
+        {console.log(page)}
+        <ComicsList 
+          pageNumber={page}
+        />
+        <FooterComics 
+          previous={handleClick}
+          next={handleClick}
+          page={page}
+        />
       </section>
     </>
   );
