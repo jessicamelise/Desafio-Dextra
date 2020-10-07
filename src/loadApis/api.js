dd382479d8ef4a17c34e3a2d3c8f5064f6123f2c
jessicamelise <jessica.melise@hotmail.com>
2020-10-07 14:41:02 -0300

    ajuste na url da api

--- src/loadApis/api.js
+++ src/loadApis/api.js
@@ -44,7 +44,7 @@ export const loadCharactersApi = async (name) => {
 
 export const loadCharactersSearchApi = async (id, offset) => {
   try {
-    const getCharactersSearchApi = await fetch(`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=8420b5759538c93703a1aa3af67a2199&hash=db6ec985bba99cf00b9fa4140ed9340e&limit=10&offset=${offset*10}&characters=${id}`)
+    const getCharactersSearchApi = await fetch(`https://gateway.marvel.com/v1/public/comics?ts=1&apikey=8420b5759538c93703a1aa3af67a2199&hash=db6ec985bba99cf00b9fa4140ed9340e&limit=10&offset=${offset*10}&characters=${id}`)
     const response = await getCharactersSearchApi.json();
     return response;
   }
