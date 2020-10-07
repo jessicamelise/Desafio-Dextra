7d6b29fce0f893112b6e67c17415cc015e066b62
jessicamelise <jessica.melise@hotmail.com>
2020-10-07 09:56:31 -0300

    implementação de pesquisa estática limitado a 10 comics por personagem

--- src/components/SearchCharacters.js
+++ src/components/SearchCharacters.js
@@ -2,28 +2,64 @@ import React, { useEffect, useState } from 'react';
 import '../App.css';
 import { Header } from "../components/Header";
 import { ReturnPage } from "../components/ReturnPage.js";
-import { loadCharactersApi } from "../loadApis/api.js";
+import { loadCharactersApi, loadCharactersSearchApi } from "../loadApis/api.js";
 // import { CharactersInformation } from "../components/CharactersInformation.js"
 
 export const SearchCharacters = ({ match }) => {
-  const [getId, setGetId] = useState([]);
+  const [getId, setGetId] = useState("");
+  const [getComics, setGetComics] = useState(null);
+  const [notFound, setNotfound] = useState("");
 
   useEffect(() => {
     loadCharactersApi(match.params.character).then((character) => {
-      let array = []
-      character.data.results.map((eachName) => {
-        array.push(eachName.id)
-      });
-      setGetId(array.join(", "))
-    });
+      if (character.data.results.length !== 0) {
+        let array = [];
+        character.data.results.map((eachName) => {
+          array.push(eachName.id);
+        });
+        setGetId(array.join(","));
+      } else {
+        setNotfound(`Results not found for ${match.params.character}`);
+      }
+    })
   }, []);
+
+  useEffect(() => {
+    if (getId) {
+      loadCharactersSearchApi(getId).then((comicsPerCharacter) => {
+        setGetComics(comicsPerCharacter.data.results);
+      });
+    }
+  }, [getId]);
 
   return (
     <>
       <Header />
       <section className="main-comics">
-        {getId}
-        {/* <CharactersInformation character={match.params.character}/> */}
+        {notFound &&
+          <p>{notFound}</p>
+        }
+        {getComics &&
+          <div className="max-width-main">
+            {getComics.map((eachComic, index) => (
+              <div className="each-comic" key={index} >
+                <img
+                  alt={eachComic.title}
+                  src={`${eachComic.thumbnail.path}.${eachComic.thumbnail.extension}`}
+                  className="thumbnails"
+                >
+                </img>
+                <p className="p-margin p-font-bold">{eachComic.title}</p>
+                {eachComic.creators.items.length === 0 ?
+                  <p className="p-margin"></p> :
+                  <p className="p-margin p-font-body">
+                    {eachComic.creators.items.map((creator, index) => <span className="list-creator" key={index}>{creator.name}/ </span>)}
+                  </p>
+                }
+              </div>
+            ))}
+          </div>
+        }
         <ReturnPage />
       </section>
     </>
--- src/loadApis/api.js
+++ src/loadApis/api.js
@@ -41,3 +41,16 @@ export const loadCharactersApi = async (name) => {
     console.error("We got a problem to fetch the information", err)
   }
 }
+
+export const loadCharactersSearchApi = async (id) => {
+  try {
+    const getCharactersSearchApi = await fetch(`http://gateway.marvel.com/v1/public/comics?ts=1&apikey=8420b5759538c93703a1aa3af67a2199&hash=db6ec985bba99cf00b9fa4140ed9340e&limit=10&offset=0&characters=${id}`)
+    const response = await getCharactersSearchApi.json();
+    return response;
+  }
+  catch (err) {
+    console.error("We got a problem to fetch the information", err)
+  }
+}
+
+
